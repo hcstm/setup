1a882e81fcbb1440495f1d47445a5ec825654745
Romain Lesur <romain.lesur@gmail.com>
2017-12-07 23:04:26 +0100

    ajouter le programme de transfo en fst

---
+++ 01-csv2fst.R
@@ -0,0 +1,25 @@
+# Vérification des packages installés -------------------------------------
+
+if(!require(data.table)) install.packages("data.table")
+if(!require(fst)) install.packages("fst")
+
+# Importation des données ----------------------------------------------------------
+# Modifier le chemin ci-dessous pour indiquer le chemin du répertoire où sont stockés les csv :
+data_dir <- "../Hackathon/data"
+
+rp <- data.table::fread(
+  file.path(data_dir, "rp_ileDeFrance.csv"), 
+  colClasses = rep("character", 46),
+  encoding = 'Latin-1')
+
+sirus <- data.table::fread(
+  file.path(data_dir, "sirus.csv"), 
+  colClasses = rep('character', 41), 
+  encoding = 'Latin-1')
+
+# Sauvegarde en fst -------------------------------------------------------
+fst::write.fst(rp, file.path(data_dir, "rp_ileDeFrance.fst"), 100)
+fst::write.fst(sirus, file.path(data_dir, "sirus.fst"), 100)
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # setup
 Des programmes à faire tourner avant le Hackathon
+
+`01-csv2fst.R` : pour sauvegarder les fichiers `csv` au format `fst`. Très utile si vous avez un `SSD` sur lequel stocker les `fst`.
